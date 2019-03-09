569a17ae8b01c71c4b6def72f5c4fd24c5324da0
Sid Wighe <swighe@appnexus.com>
2019-03-09 18:34:53 -0500

    save

--- src/component/Board.jsx
+++ src/component/Board.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import '../index.css'
 import Square from './Square'
+//import update from 'react-addons-update'
 
 export default class Board extends React.Component {
-    renderSquare(i) {
-        return <Square />
+    constructor(props) {
+        super(props)
+        this.setState({
+            cells: Array(8).fill(null)
+        })
+    }
+
+    onClick = (idx) => {
+        this.setState({
+            ...this.state,
+            //cells: update(this.state.cells, {idx: {$set: 'X'}})
+        })
+    }
+
+
+    renderSquare = (i, value, onClick) => {
+        return <Square idx={i} value={value[i]} onClick={onClick}/>
     }
 
     render() {
@@ -14,19 +30,19 @@ export default class Board extends React.Component {
             <div>
                 <div className="status">{status}</div>
                 <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
+                    {this.renderSquare(0, this.state.cells, this.onClick)}
+                    {this.renderSquare(1, this.state.cells, this.onClick)}
+                    {this.renderSquare(2, this.state.cells, this.onClick)}
                 </div>
                 <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
+                    {this.renderSquare(3, this.state.cells, this.onClick)}
+                    {this.renderSquare(4, this.state.cells, this.onClick)}
+                    {this.renderSquare(5, this.state.cells, this.onClick)}
                 </div>
                 <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
+                    {this.renderSquare(6, this.state.cells, this.onClick)}
+                    {this.renderSquare(7, this.state.cells, this.onClick)}
+                    {this.renderSquare(8, this.state.cells, this.onClick)}
                 </div>
             </div>
         )
--- src/component/Square.jsx
+++ src/component/Square.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import '../index.css';
 
-export default class Square extends React.Component {
-  render() {
-    return (
-      <button className="square">
-        {/* TODO */}
-      </button>
+export default class Square extends React.PureComponent {
+    render() {
+        return (
+          <button className="square" onClick={this.props.onClick(this.props.idx)}>
+            {this.props.value}
+          </button>
     );
   }
 }
