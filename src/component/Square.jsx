f2af7ed8ac9f1eeea133a04f1dbe71a6f9cf622a
Sid Wighe <swighe@appnexus.com>
2019-03-09 12:58:35 -0500

    Tic-tac-toe grid with base components and styles

---
+++ src/component/Square.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../index.css';
+
+export default class Square extends React.Component {
+  render() {
+    return (
+      <button className="square">
+        {/* TODO */}
+      </button>
+    );
+  }
+}
